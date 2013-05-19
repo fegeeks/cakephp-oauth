2400b10ffec710ab00442b7574cef28c0391492e
wangshijun2010 <wangshijun2010@gmail.com>
2013-05-19 10:20:30 +0800

    code formatting

--- Model/AccessToken.php
+++ Model/AccessToken.php
@@ -1,5 +1,6 @@
 <?php
 App::uses('OAuthAppModel', 'OAuth.Model');
+
 /**
  * AccessToken Model
  *
@@ -7,71 +8,77 @@ App::uses('OAuthAppModel', 'OAuth.Model');
  * @property User $User
  */
 class AccessToken extends OAuthAppModel {
-/**
- * Primary key field
- *
- * @var string
- */
-	public $primaryKey = 'oauth_token';
-/**
- * Display field
- *
- * @var string
- */
-	public $displayField = 'oauth_token';
-/**
- * Validation rules
- *
- * @var array
- */
-	public $validate = array(
-		'oauth_token' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-			),
-			'isUnique' => array(
-				'rule' => array('isUnique'),
-			)
-		),
-		'client_id' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-			),
-		),
-		'user_id' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-			),
-		),
-		'expires' => array(
-			'numeric' => array(
-				'rule' => array('numeric'),
-			),
-		),
-	);
 
-/**
- * belongsTo associations
- *
- * @var array
- */
-	public $belongsTo = array(
-		'Client' => array(
-			'className' => 'OAuth.Client',
-			'foreignKey' => 'client_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		)
-	);
+    public $useTable = 'oauth2_access_tokens';
 
-/**
- * beforeSave method to hash tokens before saving
- * 
- * @return boolean 
- */
-	public function beforeSave($options = array()) {
-		$this->data['AccessToken']['oauth_token'] = OAuthComponent::hash($this->data['AccessToken']['oauth_token']);
-		return true;
-	}
+    /**
+     * Primary key field
+     *
+     * @var string
+     */
+    public $primaryKey = 'oauth_token';
+
+    /**
+     * Display field
+     *
+     * @var string
+     */
+    public $displayField = 'oauth_token';
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public $validate = array(
+        'oauth_token' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+            ),
+            'isUnique' => array(
+                'rule' => array('isUnique'),
+            )
+        ),
+        'client_id' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+            ),
+        ),
+        'user_id' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+            ),
+        ),
+        'expires' => array(
+            'numeric' => array(
+                'rule' => array('numeric'),
+            ),
+        ),
+    );
+
+    /**
+     * belongsTo associations
+     *
+     * @var array
+     */
+    public $belongsTo = array(
+        'Client' => array(
+            'className' => 'OAuth.Client',
+            'foreignKey' => 'client_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        )
+    );
+
+    /**
+     * beforeSave method to hash tokens before saving
+     *
+     * @return boolean
+     */
+    public function beforeSave($options = array()) {
+        $this->data['AccessToken']['oauth_token'] = OAuthComponent::hash($this->data['AccessToken']['oauth_token']);
+        return true;
+    }
+
 }
